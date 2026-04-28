b85097b7149093623c3648c21faa8fe279a301f0
aleburrascano <aleburrascano123@gmail.com>
2026-04-28 12:45:24 -0400

    fix: rename to vault-init.sh so Windows npm extracts it correctly

--- bin/vault-init.js
+++ bin/vault-init.js
@@ -2,7 +2,7 @@
 'use strict';
 const { spawnSync } = require('child_process');
 const path = require('path');
-let script = path.resolve(__dirname, '..', 'vault-init');
+let script = path.resolve(__dirname, '..', 'vault-init.sh');
 if (process.platform === 'win32') script = script.replace(/\\/g, '/');
 const result = spawnSync('bash', [script, ...process.argv.slice(2)], { stdio: 'inherit' });
 process.exit(result.status || 0);
