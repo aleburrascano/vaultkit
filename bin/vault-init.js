6ff15dc279e671ac204b96ddf7c1b5ea3d4f5ead
aleburrascano <aleburrascano123@gmail.com>
2026-04-28 13:03:22 -0400

    fix: use bash -l (login shell) so PATH includes node, npm, gh

--- bin/vault-init.js
+++ bin/vault-init.js
@@ -3,5 +3,5 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 const cwd = path.resolve(__dirname, '..');
-const result = spawnSync('bash', ['vault-init.sh', ...process.argv.slice(2)], { cwd, stdio: 'inherit' });
+const result = spawnSync('bash', ['-l', 'vault-init.sh', ...process.argv.slice(2)], { cwd, stdio: 'inherit' });
 process.exit(result.status || 0);
